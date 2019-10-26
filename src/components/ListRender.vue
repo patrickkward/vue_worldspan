3b4f2b9a49d54fd1c9f77e4f6a224473334969dd
patrickkward <55601635+patrickkward@users.noreply.github.com>
2019-10-27 00:56:39 +0100

    xdx

---
+++ src/components/ListRender.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <li v-bind:key="item.message" v-for="item in items">{{ item.message }}</li>
+    <p> {{ temp }} </p>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import axios from 'axios'
+
+export default {
+  name: 'ListRender',
+  data () {
+    return {
+      temp: String
+    }
+  },
+  components: {
+  },
+  props: {
+    items: Array
+  },
+
+  created () {
+    this.printAxios()
+  },
+
+  methods: {
+    printAxios () {
+      axios.get(`http://localhost:8000/api/things/`).then(response => {
+        debugger
+        this.temp = response.data
+      }).catch(e => {
+        console.log(e)
+      })
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
